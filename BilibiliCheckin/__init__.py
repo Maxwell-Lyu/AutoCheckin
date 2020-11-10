a9a932d1b5b725d04ff2d687dd7dc5cba915d118
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-11-10 10:59:25 +0800

    add BilibiliCheckin

---
+++ BilibiliCheckin/__init__.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+import os
+import json
+import requests
+
+import azure.functions as func
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    sessdata = os.environ["BL_SESSDATA"]
+    response = requests.get("https://api.live.bilibili.com/sign/doSign", cookies={"SESSDATA": sessdata})
+    content = json.loads(response.text)
+    if content["code"] == 0:
+      logging.info("[SUCC] " + content["data"]["text"] + "," + content["data"]["specialText"])
+    else:
+      logging.error("[FAIL] " + content["message"])
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
